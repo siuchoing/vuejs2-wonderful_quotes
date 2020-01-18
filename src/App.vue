f00c9bb6a7b12cc44302f42f8c6ca3b0061a2ef1
altantic <15200582@life.hlkbu.edu.hk>
2020-01-18 16:34:55 +0800

    Create Quote and QuoteGrid component

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,14 @@
 
 <script>
     export default {
-        
+        data: function() {
+            return {
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        }
     }
 </script>
 
