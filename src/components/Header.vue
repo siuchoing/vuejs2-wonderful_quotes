1a3489cf2fa740b08425412f2e931bb9ba16fcb1
altantic <15200582@life.hlkbu.edu.hk>
2020-01-19 00:03:12 +0800

    create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12">
+            <h3>Quotes Added</h3>
+            <div class="progress">
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        // passing data from parent component (App.vue)
+        props: ['quoteCount', 'maxQuotes']
+    }
+</script>
